818c69fe001c63af9eea63697fbf96287e11885a
Jeferson M Bruno <j.bruno@ga.agr.br>
2022-07-27 12:26:32 -0300

    Inicializar projeto

---
+++ TimeTrackWebApp/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace TimeTrackWebApp
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+
+        }
+    }
+}
